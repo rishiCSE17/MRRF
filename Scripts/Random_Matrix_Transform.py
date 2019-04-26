34b39557273f366bccff0426c6477136c9ae208f
ghoshs5 <ghoshs5@lsbu.ac.uk>
2019-04-26 17:04:25 +0100

    scripts_add [OK]

---
+++ Scripts/Random_Matrix_Transform.py
@@ -0,0 +1,256 @@
+'''
+This code is written as a part of PhD project
+Purpose : from a given binary adjacency matrix this code
+    1. generates a random matrix sequence of node and link cost.
+    2. Normalises node cost to links using STEN ('Energy aware IP Routing in SDN' S.Ghosh Globecom 2018)
+    3. Calulates Reliability from past samples
+    4. plots the eadgewise reliability
+
+Project is running under : H2020 SONNET
+@author Saptarshi Ghosh
+All right Reserved : SUITE Lab, London Southbank University, UK
+'''
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+import time
+
+topo = nx.MultiDiGraph()
+
+
+def create_topo(graph, adj_matrix):
+    for i in range(len(adj_matrix)):
+        for j in range(len(adj_matrix[i])):
+            if adj_matrix[i][j] != 0:
+                graph.add_edge('node_' + str(i + 1), 'node_' + str(j + 1))
+                print(f'node_{i+1} , node_{j+1} : Added')
+    return graph
+
+
+def create_topo_with_weight(g, weighted_matrix):
+    for i in range(len(weighted_matrix)):
+        for j in range(len(weighted_matrix)):
+            if weighted_matrix.item(i, j) != 0:
+                val = round(weighted_matrix.item(i, j), 3)
+                g.add_edge('node_' + str(i + 1),
+                           'node_' + str(j + 1),
+                           weight=val,
+                           lenghth=val)
+                print(f'node_{i+1} , node_{j+1} : Added')
+                '''print(f"g.add_edge('node_{i+1}' , "
+                      f"'node_{j+1}', "
+                      f"weight={round(val,3)})")'''
+    return g
+
+
+'''
+topo=create_topo(graph=topo, adj_matrix=adj)
+nx.draw(topo, pos=nx.spring_layout(topo), with_labels=True)
+plt.show()
+'''
+
+
+def Radom_matrix_transform(adj):
+    '''creating a numpy matrix from list structure'''
+    adj_b = np.matrix(adj)
+    # print(adj_b)
+
+    '''creating random matrix'''
+    rand = np.random.rand(adj_b.shape[0], adj_b.shape[1])
+    # pd.DataFrame(rand)
+
+    '''element wise multiplication'''
+    adj_e = np.multiply(rand, adj_b)
+    # pd.DataFrame(adj_e)
+
+    '''calculating Adj_n'''
+    x = np.multiply(np.random.rand(
+        adj_b.shape[0], adj_b.shape[1]),
+        np.identity(adj_b.shape[0])
+    )
+    # pd.DataFrame(x)
+
+    adj_n = x + adj_e
+    # pd.DataFrame(adj_n)
+    return adj_b, adj_n
+
+
+def get_non_zero_index(adj):
+    ret = []
+    for i in range(adj.shape[0]):
+        row = []
+        for j in range(adj.shape[1]):
+            if adj.item(i, j) != 0:
+                row.append(j)
+        ret.append(row)
+    return np.matrix(ret)
+
+
+def distribute_rand_over_nz_row(mat):
+    ret = []
+    for row in range(mat.shape[1]):
+        temp = []
+        for i in range(len(mat.item(row))):
+            temp.append(random.randint(0, 100))
+        r_sum = sum(temp)
+        for i in range(len(temp)):
+            temp[i] = round((temp[i] / r_sum), 3)
+        ret.append(temp)
+    return ret
+
+
+def generate_affinity(nz_indx_mat, nz_rand_mat, adj_n):
+    ret = []
+    for i in range(adj_n.shape[0]):
+        n_util = adj_n.item(i, i)
+        row = []
+        k = 0
+        for j in range(adj_n.shape[1]):
+            if j in nz_indx_mat.item(i):
+                offset = nz_rand_mat[i][k] * n_util
+                row.append(offset)
+                k += 1
+            else:
+                row.append(0)
+        row[i] = -n_util
+        ret.append(row)
+    return ret
+
+
+def get_adj_s(adj):
+    adj_b, adj_n = Radom_matrix_transform(adj)
+    nz_indx_mat = get_non_zero_index(adj_b)
+    nz_rand_mat = distribute_rand_over_nz_row(get_non_zero_index(adj_b))
+    affifity_mat = generate_affinity(nz_indx_mat, nz_rand_mat, adj_n)
+    adj_s = affifity_mat + adj_n
+
+    print(adj_s)
+    return adj_s
+
+
+'''returns the weights of edges'''
+
+
+def get_edge_weight(adj_s):
+    mat = np.matrix(adj_s)
+    arr = mat.reshape(mat.shape[0] * mat.shape[1])
+    indx = np.where(arr != 0)[1]
+    vect = np.take(arr, indx)
+    return np.matrix.tolist(vect)
+
+
+'''calculates the lement wise median ans SD of a given matrix'''
+
+
+def get_median_sd(sampled_matrix):
+    import statistics as stat
+    median = []
+    sd = []
+    for node_history in sampled_matrix:
+        median.append(stat.median(node_history))
+        sd.append(stat.stdev(node_history))
+    return (median, sd)
+
+
+def get_Quality(s_median, s_sd, w1=0.5, w2=0.5):
+    q = []
+    for i in range(len(s_median)):
+        q.append(w1 * s_median[i] + w2 * s_sd[i])
+    return q
+
+
+def get_Reliability(quality_vct):
+    R = []
+    for q in quality_vct:
+        R.append((q - min(quality_vct)) / (max(quality_vct) - min(quality_vct)))
+
+    return R
+
+
+def calculate_reliabiliy(sample_space, adj_s):
+    edge_sample = get_edge_weight(adj_s)
+    # print(edge_sample[0])
+    r_vector = [0] * len(edge_sample[0])
+
+    if len(sample_space) == 10:
+        sample_space.pop(0)
+    sample_space.append(edge_sample[0])
+    # print(pd.DataFrame(sample_space).shape)
+
+    temp_mat = np.matrix(sample_space)
+    temp_mat1 = np.transpose(temp_mat)
+    sample_space = np.matrix.tolist(temp_mat1)
+    # print(pd.DataFrame(sample_space))
+    # print(len(sample_space[0]))
+
+    #time.sleep(1)
+
+    if (len(sample_space[0]) >= 2):
+        sample_median, sample_sd = get_median_sd(sample_space)
+        q_vector = get_Quality(sample_median, sample_sd, w1=0.4, w2=0.6)
+        r_vector = get_Reliability(q_vector)
+
+    return r_vector
+
+    # return None
+
+
+def loop(adj, topo_s):
+    weighted_sample_space = []
+
+    while True:
+        adj_s = get_adj_s(adj)
+        # print(pd.DataFrame(adj_s))
+        topo_s.clear()
+        topo_s = create_topo_with_weight(topo_s, adj_s)
+        plt.ion()
+
+        fig_topo = plt.figure('Topology')
+        fig_topo = plt.title('Topology')
+        fig_topo = plt.ion()
+        fig_topo = plt.pause(2)
+        fig_topo = plt.clf()
+        nx.draw(topo_s, pos=nx.spectral_layout(topo_s), with_labels=True)
+
+        fig_reliab = plt.figure('Reliability')
+        fig_reliab = plt.title('Relaibility')
+        fig_reliab = plt.ion()
+        fig_reliab = plt.pause(2)
+        fig_reliab = plt.clf()
+
+        r_vector = calculate_reliabiliy(weighted_sample_space, adj_s)
+
+
+        plt.bar(np.arange(len(topo_s.edges)),
+                r_vector,
+                label='reliability',
+                alpha=1)
+
+        # fig_topo.show()
+        # fig_reliab.show()
+        plt.show()
+
+
+def main():
+    print('Adjacency Matrix Initiaged... ')
+    adj = [[0, 1, 1, 0, 0, 1],
+           [1, 0, 0, 0, 0, 1],
+           [1, 0, 0, 1, 1, 0],
+           [0, 0, 1, 0, 1, 1],
+           [0, 0, 1, 1, 0, 0],
+           [1, 1, 0, 1, 0, 0]]
+    print('[1/3] Binary Adjacency Matrix Initiaged... ')
+    time.sleep(1)
+    print(adj)
+    topo_s = nx.MultiDiGraph()
+    print('[2/3] Topology Graph Initiated...')
+    time.sleep(1)
+    print('[3/3] Initialising... Entering indefinite loop, Close to Terminate...')
+    time.sleep(1)
+    loop(adj, topo_s)
+
+
+main()
---
+++ Scripts/Toposense_SPA.py
@@ -0,0 +1,405 @@
+'''
+    opendaylight - python integrator master file
+    author : Saptarshi Ghosh
+    features :
+        1. fetaches topology from ODL (API : network-topology | GET)
+        2. fetches ovs flow information (API : inverntory | GET)
+        3. writes mac forwaridding information to MySQL DB
+
+'''
+import pprint
+import requests
+import json
+import matplotlib.pyplot as plt
+import networkx as nx
+import sqlite3
+import pymysql
+
+''' -------------------------------------------------------------------------------------------------
+    class definitions
+    -------------------------------------------------------------------------------------------------'''
+
+
+class host:
+    def __init__(self, node_id, h_id, mac, ip):
+        self.node_id = node_id
+        self.h_id = h_id
+        self.mac = mac
+        self.ip = ip
+        self.host_data = [self.node_id, self.h_id, self.mac, self.ip]
+
+    def get_host(self):
+        return self.host_data
+
+
+class flow_entry:
+    def __init__(self, source="", destination="", out_port=0, prio=0, h_to=0, p_count=0, b_count=0, duration=0):
+        self.source = source
+        self.destination = destination
+        self.out_port = out_port
+        self.prio = prio
+        self.h_to = h_to
+        self.p_count = p_count
+        self.b_count = b_count
+        self.duration = duration
+
+    def get_flow(self):
+        return [self.source, self.destination, self.out_port, self.prio, self.h_to, self.p_count, self.b_count,
+                self.duration]
+
+
+''' -------------------------------------------------------------------------------------------------
+    list definitions
+    -------------------------------------------------------------------------------------------------'''
+
+host_list = []  # list of host object
+h_list = []  # list of host names
+node_list = []  # list of nodes (host + switches)
+switch_list = []  # list of switches
+edge_list = []  # list of edges format : (node1,node2)
+adj_mat = [[]]  # adjacency matrix of the graph
+switch_list = []
+flow_tab_list = []
+
+'''------------------------------------------------------------------------------------------------
+   Shortest path algo 
+   -----------------------------------------------------------------------------------------------'''
+import networkx as nx
+import matplotlib.pyplot as plt_spa
+import math
+import drawnow
+import random
+from matplotlib import style
+
+# style.use('fivethirtyeight')
+
+fig_spa = plt_spa.figure()
+route_list = []
+g_list = []
+
+
+def gen_random_weight(edge_count):
+    ret_weigt_list = []
+    for i in range(0, edge_count):
+        ret_weigt_list.append(random.randrange(5, edge_count))
+    return ret_weigt_list
+
+
+def create_graph(adj):
+    g = nx.Graph()
+    g.add_weighted_edges_from(adj)
+    return g
+
+
+# nx.draw(g, nodcolor='r',edgecolor='b')
+# plt.show()
+
+def get_node_seq(lst_rt):
+    ret = []
+    for i in range(0, len(lst_rt) - 1):
+        ret.append((lst_rt[i], lst_rt[i + 1]))
+    return ret
+
+
+def gen_route_graphs(lst_edge):
+    g_temp = nx.Graph()
+    g_temp.add_edges_from(lst_edge)
+    return g_temp
+
+
+def plot_all_sp(g):
+    pos = nx.spring_layout(g)  # positions for all nodes
+
+    for node_s in g.nodes:
+        for node_d in g.nodes:
+            if node_s < node_d and node_s != node_d:
+                print('R : ' + str(node_s) + ' <--> ' + str(node_d))
+                n_lst = nx.dijkstra_path(g, node_s, node_d)
+                e_lst = get_node_seq(n_lst)
+                route_list.append(gen_route_graphs(e_lst))
+                print('\t : ', n_lst)
+                print('\t : ', e_lst)
+
+    axes = math.sqrt(len(route_list))
+    sp_list = []
+    k = 1
+    for g in route_list:
+        sp_spa = fig_spa.add_subplot(axes + 1, axes + 1, k)
+        nx.draw_networkx(g,
+                         pos=nx.spring_layout(g),
+                         edge_labes=nx.get_edge_attributes(g, 'weight'),
+                         with_labels=True,
+                         font_size=12,
+                         font_color='black',
+                         style='dashed',
+                         axes='off',
+                         edge_color='r',
+                         node_color='g'
+                         )
+        plt_spa.plot()
+
+        sp_list.append(sp_spa)
+        k = k + 1
+
+    plt_spa.suptitle("All pair Dijkstrs's algo", )
+    plt_spa.show()
+
+
+def gen_weighted_list(edge_list, node_list):
+    w_list = gen_random_weight(len(edge_list))
+    index = 0
+    adj = []
+    for i in edge_list:
+        print(i[0], i[1], w_list[index])
+        # dj.append(i,j,w_list[index])
+        index += 1
+    '''            
+    adj = [(1, 2, w_list[0]), (1, 3, w_list[1]), (1, 4, w_list[2]),
+           (2, 3, w_list[3]),
+           (3, 5, w_list[4]),
+           (4, 5, w_list[5])]
+    '''
+    return adj
+
+
+def spa_main(g):
+    global w_list
+    global fig_spa
+
+    w_list = []
+    fig_spa.clear()
+    g_list.append(nx.Graph)
+    adj = gen_weighted_list(g.edges, g.nodes)
+    print('creating graph...')
+    # g=create_graph(adj)
+    print('Running Shortest path algo...')
+    plot_all_sp(g)
+
+
+'''-------------------------------------------------------------------------------------------------'''
+
+''' -------------------------------------------------------------------------------------------------
+    mysql database integration
+    -------------------------------------------------------------------------------------------------'''
+
+host_ip = input(r'Enter MySQL Server IP : ')
+un = input('user name : ')
+pw = input(r'Enter Password')
+db = input(r'Enter Schema name')
+
+db = pymysql.connect(host=host_ip, user=un, passwd=pw, db=db)
+
+
+def create_tab():
+    db.query(
+        "CREATE TABLE IF NOT EXISTS flow_table (switch varchar(45), \
+                                                source varchar(45), \
+                                                destination varchar(45), \
+                                                output_port REAL, \
+                                                priority REAL, \
+                                                hard_timeout REAL, \
+                                                packet_count REAL, \
+                                                byte_count REAL, \
+                                                duration REAL)"
+    )
+
+    db.query("commit")
+
+
+def clear_db():
+    db.query("delete from flow_table")
+    db.query("commit")
+
+
+def insert_data(sw, flow_entry):
+    src = flow_entry[0]
+    dst = flow_entry[1]
+    o_port = flow_entry[2]
+    prio = flow_entry[3]
+    h_to = flow_entry[4]
+    p_cnt = flow_entry[5]
+    b_cnt = flow_entry[6]
+    dur = flow_entry[7]
+
+    '''
+    print("INSERT INTO flow_table VALUES('" + sw + "','" + src + "','" + dst + "'," \
+                                                  + str(o_port) + "," + str(prio) + "," \
+                                                  + str(h_to) + "," + str(p_cnt) +"," \
+                                                  + str(b_cnt) + "," + str(dur) + ")" )
+    '''
+    db.query("INSERT INTO flow_table VALUES('" + sw + "','" + src + "','" + dst + "'," \
+             + str(o_port) + "," + str(prio) + "," \
+             + str(h_to) + "," + str(p_cnt) + "," \
+             + str(b_cnt) + "," + str(dur) + ")")
+    db.query("commit")
+
+
+# init_db('shellmon')
+# create_tab()
+
+''' -------------------------------------------------------------------------------------------------
+    ODL integration : topology fetch 
+    -------------------------------------------------------------------------------------------------'''
+
+
+# populates the lists from the RESTfull api content processing
+def generate_lists(cont_ip):
+    url = 'http://' + cont_ip + ':8181/restconf/operational/network-topology:network-topology/topology/flow:1'
+    get_topo = requests.get(url, auth=('admin', 'admin'))
+    get_topo_json = get_topo.json()
+    elem = get_topo_json['topology'][0]
+
+    for x in elem['node']:
+        node_id = str(x['node-id'])
+        if (node_id.__contains__('host')):
+            # print(node_id)
+            temp = x['host-tracker-service:addresses'][0]
+            # print('\t id:',temp['id'], ' mac:',temp['mac'], ' ip: ',temp['ip'])
+            host_list.append(host(node_id, temp['id'], temp['mac'], temp['ip']))
+            node_list.append(node_id)
+            h_list.append(node_id)
+
+        if (node_id.__contains__('openflow')):
+            # print(node_id)
+            switch_list.append(node_id)
+            node_list.append(node_id)
+
+    for y in elem['link']:
+        # print(y['link-id'])
+        # print('\t',y['source']['source-node'],' ---> ',y['destination']['dest-node'])
+        edge_list.append((y['source']['source-node'], y['destination']['dest-node']))
+
+
+'''----------------------------------------------------------------------------------------------------------'''
+
+
+def show_discovery_message():
+    print('hosts discovered...')
+    for host in host_list:
+        print(host.get_host())
+
+    print('switches discovered...')
+    for switch in switch_list:
+        print(switch)
+
+    print('links discovered...')
+    for link in edge_list:
+        print(link)
+
+    print('nodes discovered...')
+    for node in node_list:
+        print(node)
+
+
+'''-------------------------------------------------------------------------------------------------------------'''
+
+
+def build_adj_mat():
+    print('from build_adj_mat()')
+
+
+'''-------------------------------------------------------------------------------------------------------------'''
+
+
+def generate_graph():
+    g = nx.Graph()
+    g.add_nodes_from(h_list, color='green')
+    g.add_nodes_from(switch_list, color='blue')
+    g.add_edges_from(edge_list, color='green')
+
+    nx.draw_networkx(g, with_labels=True, font_size=10, font_color='blue', style='dashed')
+    # plt.axes('off')
+    'plot _control'
+    # plt.show()
+    return g
+
+
+'''--------------------------------------------------------------------------------------------------------------'''
+
+''' -------------------------------------------------------------------------------------------------
+    ODL integration : flow tables fetch 
+    -------------------------------------------------------------------------------------------------'''
+
+
+def generate_flow_tab(switch, cont_ip):
+    url = 'http://' + cont_ip + ':8181/restconf/operational/opendaylight-inventory:nodes/node/' + switch + '/flow-node-inventory:table/0'
+    get_flow_tab = requests.get(url, auth=('admin', 'admin'))
+    get_flow_tab_json = get_flow_tab.json()
+
+    temp_flow_tab = []
+
+    for flow in get_flow_tab_json['flow-node-inventory:table'][0]['flow']:
+        if 'ethernet-match' in flow['match'] and 'ethernet-source' in flow['match']['ethernet-match']:
+            s_addr = flow['match']['ethernet-match']['ethernet-source']['address']
+            d_addr = flow['match']['ethernet-match']['ethernet-destination']['address']
+            out_port = flow['instructions']['instruction'][0]['apply-actions']['action'][0]['output-action'][
+                'output-node-connector']
+            prio = flow['priority']
+            hto = flow['hard-timeout']
+            p_count = flow['opendaylight-flow-statistics:flow-statistics']['packet-count']
+            b_count = flow['opendaylight-flow-statistics:flow-statistics']['byte-count']
+            duration = flow['opendaylight-flow-statistics:flow-statistics']['duration']['second']
+
+            temp_flow_tab.append(flow_entry(s_addr, d_addr, out_port, prio, hto, p_count, b_count, duration))
+
+            '''
+            print(' source=',s_addr,
+                  ' destination=',d_addr,
+                  ' output=',out_port,
+                  'prio=',prio,
+                  'hto=',hto,
+                  'p_count',p_count,
+                  'b_count',b_count,
+                  'duration',duration
+                  )
+
+            '''
+        # endif
+    # endloop
+
+    if len(temp_flow_tab) == 0:
+        temp_flow_tab.append(flow_entry())
+    if switch not in flow_tab_list:
+        flow_tab_list.append([switch, temp_flow_tab])
+
+
+def show_flow_tab():
+    create_tab()
+    clear_db()
+    for flow_tab in flow_tab_list:
+        print(flow_tab[0])
+        for flow_entry in flow_tab[1]:
+            fe = flow_entry.get_flow()
+            print(fe)
+            insert_data(flow_tab[0], fe)
+
+
+def main_flow_fetch(switch_l, cont_ip):
+    '''cont_ip=input('Enter Controller IP : ')'''
+    switch_list = switch_l
+    for switch in switch_list:
+        generate_flow_tab(switch, cont_ip)
+
+    show_flow_tab()
+
+
+''' -------------------------------------------------------------------------------------------------
+    main function  
+    -------------------------------------------------------------------------------------------------'''
+
+
+def main():
+    cont_ip = input('enter controller ip : ')
+    generate_lists(cont_ip)
+    show_discovery_message()
+    # build_adj_mat()
+    main_flow_fetch(switch_list, cont_ip)
+    g = generate_graph()
+    spa_main(g)
+
+
+''' -------------------------------------------------------------------------------------------------
+    call starts here !! 
+    -------------------------------------------------------------------------------------------------'''
+
+main()
